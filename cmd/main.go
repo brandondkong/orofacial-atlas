2cf96f0e8febc95f2d957d81817d9d36116c4f0b
Brandon <kongbrandon0@gmail.com>
2026-07-06 18:31:35 -0500

    chore: added colored outputs

--- cmd/main.go
+++ cmd/main.go
@@ -4,25 +4,23 @@ import (
 	"fmt"
 
 	"github.com/brandondkong/orofacial-atlas/pkg/config"
-	"github.com/brandondkong/orofacial-atlas/pkg/input"
+	"github.com/fatih/color"
 )
 
 func main() {
-	println("Initializing Orofacial Atlas")
-
+	color.New(color.Bold, color.FgCyan).Println("\nInitializing Orofacial Atlas...")
+	fmt.Println("Looking for configs...")
 	if config.DoesConfigExist() {
 		// load configs
 		println("Loading existing configs")
 	} else {
 		// onboard new configs
-		println("First time using Orofacial Atlas? Welcome!")
-
-		char, err := input.PromptSingleCharacter("Are you excited? (y/n): ")
+		color.Yellow("No configs found")
+		fmt.Print("Booting config wizard...\n\n")
+		err := config.PromptNewConfigs()
 		if err != nil {
-			panic(err)
+			println(err.Error())
 		}
-
-		fmt.Printf("%c\n", char)
 	}
 
 	// load data to RAM based on configs
