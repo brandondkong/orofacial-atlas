f3c2fd46389fdc0257bd4dd2a7ba33b1f8f04487
Brandon <kongbrandon0@gmail.com>
2026-07-06 18:30:41 -0500

    feat(input): added SIGINT handling for raw terminal

--- pkg/input/reader.go
+++ pkg/input/reader.go
@@ -2,13 +2,16 @@ package input
 
 import (
 	"bufio"
-	"fmt"
+	"errors"
 	"os"
+	"strings"
 
 	"golang.org/x/term"
 )
 
-func ReadSingleCharacter(buffer *rune) error {
+var ErrInterrupted error = errors.New("interrupted")
+
+func ReadSingleCharacter(buffer *string) error {
 	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
 	if err != nil {
 		panic(err)
@@ -20,19 +23,76 @@ func ReadSingleCharacter(buffer *rune) error {
 	if err != nil {
 		return err
 	}
+	if char == 0x03 {
+		return ErrInterrupted
+	}
 
-	*buffer = char
+	*buffer = string(char)
 	return nil
 }
 
-func PromptSingleCharacter(prompt string) (rune, error) {
-	var char rune
+func PromptSingleCharacter(prompt string) (string, error) {
+	var char string
 	print(prompt)
 	err := ReadSingleCharacter(&char)
 	return char, err
 }
 
+func PromptBooleanAnswer(prompt string, trueKey string, falseKey string) (bool, error) {
+	var char string
+	var choice bool
+
+	trueKey = strings.ToLower(trueKey)
+	falseKey = strings.ToLower(falseKey)
+
+	print(prompt)
+	for (char != trueKey && char != falseKey) {
+		err := ReadSingleCharacter(&char)
+		if err != nil {
+			return false, err
+		}
+		char = strings.ToLower(char)
+		if char == trueKey {
+			choice = true
+		}
+
+		if char == falseKey {
+			choice = false
+		}
+	}
+
+	if choice {
+		println(trueKey)
+	} else {
+		println(falseKey)
+	}
+
+	return choice, nil
+}
+
+func PromptYesNoAnswer(prompt string) (bool, error) {
+	return PromptBooleanAnswer(prompt, "y", "n")
+}
+
 func ReadString(buffer *[]byte) error {
-	fmt.Scan()
+	reader := bufio.NewReader(os.Stdin)
+	char, _, err := reader.ReadLine()
+	if err != nil {
+		return err
+	}
+
+	*buffer = char
 	return nil
+}
+
+func PromptString(prompt string) (string, error) {
+	var buffer []byte
+	print(prompt)
+
+	err := ReadString(&buffer)
+	if err != nil {
+		return "", err
+	}
+
+	return string(buffer), nil
 }
